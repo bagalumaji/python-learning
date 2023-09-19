c158f187c722cc962cfd1a99cc2db42a82457c96
bagalumaji <bagalumaji@gmail.com>
2023-09-19 22:35:33 +0530

    print 1 to 10 numbers

---
+++ basic/print1to10.py
@@ -0,0 +1,12 @@
+# print 1 to 10 numbers
+for i in range(1, 11):
+    print(i)
+
+# print 10 to 1 numbers
+for i in range(10, 0, -1):
+    print(i)
+
+# print only even numbers from 1 to 10
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
