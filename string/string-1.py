152f137e4e332c4af6c1053ee68e695914901e66
umaji bagal <bagalumaji@gmail.com>
2024-11-17 21:30:12 +0530

    string methods

---
+++ string/string-1.py
@@ -0,0 +1,14 @@
+hello_world="hello world"
+
+#print on console
+print(hello_world)
+
+#len of string
+print(len(hello_world))
+
+# upper case
+print(hello_world.upper())
+
+#to lower case
+print(hello_world.lower())
+
